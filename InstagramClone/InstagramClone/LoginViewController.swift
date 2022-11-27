ba8b97abd28893a73f38f59a88872247648eb3b4
bob.kim <bob.kim@mvlchain.io>
2022-11-27 18:41:12 +0900

    feat: add login view controller

---
+++ InstagramClone/InstagramClone/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  InstagramClone
+//
+//  Created by Bob on 2022/11/27.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    var email = String()
+    var password = String()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.email = text
+    }
+    
+    @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.password = text
+    }
+    
+    @IBAction func loginButtonDidTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonDidTap(_ sender: UIButton) {
+    }
+    
+}
