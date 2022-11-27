9f423aa5a48dadbc0ab30c152cdf7f747ae9e1c4
bob.kim <bob.kim@mvlchain.io>
2022-11-27 19:28:50 +0900

    feat: add register view controller

--- InstagramClone/InstagramClone/RegisterViewController.swift
+++ InstagramClone/InstagramClone/RegisterViewController.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var isValidEmail = false
+    
+    var isValidName = false
+    
+    var isValidNickname = false
+    
+    var isValidPassword = false
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nicknameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var textFields: [UITextField] {
+        [emailTextField, nameTextField, nicknameTextField, passwordTextField]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTextField()
     }
-    */
+    
+    @objc
+    func textFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        
+        switch sender {
+        case emailTextField:
+            print("email")
+        case nameTextField:
+            print("name")
+        case nicknameTextField:
+            print("nickname")
+        case passwordTextField:
+            print("password")
+        default:
+            fatalError("Missing TextField...")
+        }
+    }
+    
+    private func setupTextField() {
+        textFields.forEach { tf in tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)}
 
+    }
 }
