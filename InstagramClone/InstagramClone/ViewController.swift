e9fbe0c1fd68dd43dcef5989a73d469bc5a35bd3
bob.kim <bob.kim@mvlchain.io>
2022-11-27 18:04:39 +0900

    initiallize instagram clone project

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Bob on 2022/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
